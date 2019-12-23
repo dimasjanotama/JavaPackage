9eec0bb56efa44cd9a4307d6426b4a1cea6e7b4e
dimasjanotama <dimasprasidha@gmail.com>
2019-12-23 14:17:13 +0700

    printing dan alur eksekusi java

---
+++ src/com/ucup/Main.java
@@ -0,0 +1,12 @@
+package com.ucup;
+
+/**
+ * Main
+ */
+public class Main {
+    public static void main(String[] args) {
+        String x = "TEST SUCCESFUL";
+        System.out.println(x);
+    }
+    
+}
