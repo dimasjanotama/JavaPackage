2bbddcefe8835988ce4cad08eaedb867f8bf8423
dimasjanotama <dimasprasidha@gmail.com>
2019-12-23 14:35:02 +0700

    printing dan alur eksekusi java

--- src/com/printing/Main.java
+++ src/com/printing/Main.java
@@ -2,6 +2,16 @@ package com.printing;
 
 public class Main {
     public static void main(String[] args){
+        // Eksekusi baris per baris
+        // print = eksekusi lgsung, println= pakai space, manual pakai \n
+        // d = tipe data decimal
         System.out.println("Test Succesfull");
+        System.out.println("Test Succesfull line 2");
+        System.out.print("Test Succesfull line 3");
+        System.out.println("Test Succesfull line 4");
+        System.out.print("Test Succesfull line 5 \n");
+        System.out.println("Test Succesfull line 6");
+
+        System.out.printf("Test Succesfull line 7 %d", 212);
     }
 }
