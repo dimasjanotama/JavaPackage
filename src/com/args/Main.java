66596ec461d8c0a3417fc4fb8f4bd46074e9af40
dimasjanotama <dimasprasidha@gmail.com>
2019-12-24 15:40:36 +0700

    strings args

--- src/com/args/Main.java
+++ src/com/args/Main.java
@@ -2,7 +2,11 @@ package com.args;
 
 public class Main{
     public static void main(String[] args){
-        System.out.println(args)
-        System.out.println("Test Succesful")
+        System.out.println("Halo "+args[0]+" Succesful");
+        System.out.println(args[1]);
     }
 }
+
+// in bin run Main.class --> java com.args.Main Dimas Janotama
+// Output : Halo Dimas Succesful
+//          Janotama
--- src/com/ucup/Main.java
+++ src/com/ucup/Main.java
@@ -8,5 +8,11 @@ public class Main {
         String x = "TEST SUCCESFUL";
         System.out.println(x);
     }
-    
 }
+
+// How to make package and class to run by JVM :
+// 1. Buat folder bin dan src --> com --> buat folder (nama)
+// 2. Buat Main.java di folder tsb
+// 3. Compile dengan java compiler : arahkan ke folder src, run:
+//          javac [folder letak Main.java] -d [direktori dimana com dll mau disimpan (folder class bin)]
+//          javac com/ucup/Main.java -d ../bin 
