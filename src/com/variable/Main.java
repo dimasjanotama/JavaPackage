7586ce82437515343be0157259f0a9e39418531a
dimasjanotama <dimasprasidha@gmail.com>
2019-12-25 18:54:49 +0700

    variable assignment declaration

--- src/com/variable/Main.java
+++ src/com/variable/Main.java
@@ -2,6 +2,16 @@ package com.variable;
 
 public class Main{
     public static void main(String[] args){
-        
+        int x = 10; //Assignment
+        System.out.println("nilai x = "+ x);
+        x = 20;
+        System.out.println("nilai baru x = "+ x);
+
+        // declaration
+        int y;
+        y = 30;
+        System.out.println("nilai y = "+ y);
+
+
     }
 }
