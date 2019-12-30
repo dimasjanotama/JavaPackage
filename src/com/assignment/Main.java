99cb6a05bb5136012dbf9d80d1b774466df4e91e
dimasjanotama <dimasprasidha@gmail.com>
2019-12-30 19:44:42 +0700

    assignment operator

---
+++ src/com/assignment/Main.java
@@ -0,0 +1,32 @@
+package com.assignment;
+
+public class Main{
+    public static void main(String[] args){
+        // operator assignment
+
+        // Assignment operator jumlah
+        int a = 1;
+        a += 10; // a = a + 10
+        System.out.println(a);
+
+        // Assignment operator kurang
+        int b = 100;
+        b -= 25; // b = b - 25
+        System.out.println(b);
+
+        // Assignment operator kali
+        int c = 100;
+        c *= 20; // c = c * 25
+        System.out.println(c);
+
+        // Assignment operator bagi
+        int d = 100;
+        d /= 25; // d = d / 25
+        System.out.println(d);
+
+        // Assignment operator modulus
+        int e = 10;
+        e %= 7; // e = e % 25
+        System.out.println(e);
+    }
+}
