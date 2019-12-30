92768b85581cd3d216432f0453ab0eb076c0e01e
dimasjanotama <dimasprasidha@gmail.com>
2019-12-30 19:35:58 +0700

    unary operator

---
+++ src/com/unary/Main.java
@@ -0,0 +1,33 @@
+package com.unary;
+
+public class Main{
+    public static void main(String[] args){
+        // Unary = operasi yang dilakukan pada 1 variabel saja
+
+        // unary plus minus
+        int angka = 1;
+        System.out.printf("unary '+', %d menjadi %d\n", angka,+angka);
+        System.out.printf("unary '-', %d menjadi %d\n", angka,-angka);
+
+        // unary increment dan decrement
+        int angka2 = 10;
+        angka2++;
+        System.out.println(angka2);
+
+        int angka3 = 10;
+        angka3--;
+        System.out.println(angka3);
+
+        int a=5;
+        System.out.printf("nilai dengan '++' prefix menjadi = %d \n", ++a);
+        int b=5;
+        System.out.printf("nilai dengan '++' postfix menjadi = %d \n", b++);
+        // KENAPA BEGITU? karena ++a artinya dia menambahkan baru ditampilkan, sdgkan a++ ditampilkan dulu 
+        // baru dijumlahkan
+
+        // unary boolean
+        boolean ucup = true;
+        System.out.println("nilai boolean = "+ ucup);
+        System.out.println("nilai boolean = "+ !ucup);
+    }
+}
