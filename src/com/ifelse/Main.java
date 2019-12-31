7193ba8d89244e255a98eeb88d192331e854cc95
dimasjanotama <dimasprasidha@gmail.com>
2019-12-31 21:23:19 +0700

    if nested

--- src/com/ifelse/Main.java
+++ src/com/ifelse/Main.java
@@ -2,9 +2,9 @@ package com.ifelse;
 
 public class Main{
     public static void main(String[] args){
-        int a = 10;
-
+        int a,b;
         // IF ELSE 
+        a = 10;
         System.out.println("------------IF ELSE");
         if(a==5){
             System.out.println("Selesai");
@@ -26,10 +26,24 @@ public class Main{
         }
         System.out.println("ini adalah AKHIR program");
 
-        // IF NESTED
+        // IF NESTED (IF didalam IF)
         System.out.println("--------------IF NESTED");
-
-
+        a = 5;
+        b = 20;
+        System.out.println("ini adalah AWAL program");
+        if(a==5){
+            if(b==20){
+                System.out.println("ini action 1");
+            } else {
+                System.out.println("ini default 1");
+            }
+        } else if(a==10){
+            System.out.println("ini action 2");
+        } else {
+            System.out.println("ini default 3");
+        }
+        System.out.println("ini adalah AKHIR program");
+        
     }
     
 }
