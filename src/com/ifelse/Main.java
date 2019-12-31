1d3c06dc7fc5478b31e0615eccc48f2360c4c96a
dimasjanotama <dimasprasidha@gmail.com>
2019-12-31 22:58:22 +0700

    if else

---
+++ src/com/ifelse/Main.java
@@ -0,0 +1,14 @@
+package com.ifelse;
+
+public class Main{
+    public static void main(String[] args){
+        int a = 10;
+
+        if(a==5){
+            System.out.println("Selesai");
+        } else {
+            System.out.println("This is a branch");
+        }
+        System.out.println("selesai");
+    }
+}
