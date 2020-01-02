4827a3d6ba42f9e76bc696f6c4929a6ade3d99d1
dimasjanotama <dimasprasidha@gmail.com>
2020-01-02 19:11:08 +0700

    calculator switch case

--- src/com/calculator/Main.java
+++ src/com/calculator/Main.java
@@ -5,43 +5,79 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
         // a operator b
-        // IF ELSE CALCULATOR
+        // IF ELSE CALCULATORR
+
+        // Scanner inputUser;
+        // float a,b,hasil;
+        // char operator;
+
+        // inputUser = new Scanner(System.in);
+
+        // System.out.print("Nilai a : ");
+        // a = inputUser.nextFloat();
+        // System.out.print("Operator  : ");
+        // operator = inputUser.next().charAt(0); //charAt(0) artinya mengambil karkter paling depan makanya 0
+        // System.out.print("Nilai b : ");
+        // b = inputUser.nextFloat();
+
+        // System.out.println("input user: "+a +" "+operator+ " "+b);
+
+        // // operator * / + -
+        // if (operator == '+'){
+        //     hasil = a+b;
+        //     System.out.println("hasil = " + hasil);
+        // } else if(operator == '-'){
+        //     hasil = a-b;
+        //     System.out.println("hasil = " + hasil);
+        // } else if(operator == '*'){
+        //     hasil = a*b;
+        //     System.out.println("hasil = " + hasil);
+        // } else if(operator== '/'){
+        //     if(b==0){
+        //         System.out.println("pembagi nol menghasilkan tak hingga");
+        //     }else{
+        //         hasil = a/b;
+        //         System.out.println("hasil = " + hasil);
+        //     }
+        // } else {
+        //     System.out.println("-------------OPERATOR TIDAK DITEMUKAN");
+        // }
+
+        // SWITCH CASE CALCULATOR
 
         Scanner inputUser;
         float a,b,hasil;
-        char operator;
+        String operator;
 
         inputUser = new Scanner(System.in);
 
-        System.out.print("Nilai a : ");
+        System.out.print("nilai a = ");
         a = inputUser.nextFloat();
-        System.out.print("Operator  : ");
-        operator = inputUser.next().charAt(0); //charAt(0) artinya mengambil karkter paling depan makanya 0
-        System.out.print("Nilai b : ");
+        System.out.print("operator = ");
+        operator = inputUser.next();
+        System.out.print("nilai b = ");
         b = inputUser.nextFloat();
-
-        System.out.println("input user: "+a +" "+operator+ " "+b);
-
-        // operator * / + -
-        if (operator == '+'){
-            hasil = a+b;
-            System.out.println("hasil = " + hasil);
-        } else if(operator == '-'){
-            hasil = a-b;
-            System.out.println("hasil = " + hasil);
-        } else if(operator == '*'){
-            hasil = a*b;
-            System.out.println("hasil = " + hasil);
-        } else if(operator== '/'){
-            if(b==0){
-                System.out.println("pembagi nol menghasilkan tak hingga");
-            }else{
+        
+        switch(operator){
+            case "+":
+                hasil = a+b;
+                System.out.println("hasil = "+hasil);
+                break;
+            case "-":
+                hasil = a-b;
+                System.out.println("hasil = "+hasil);
+                break;
+            case "*":
+                hasil = a*b;
+                System.out.println("hasil = "+hasil);
+                break;
+            case "/":
                 hasil = a/b;
-                System.out.println("hasil = " + hasil);
-            }
-        } else {
-            System.out.println("-------------OPERATOR TIDAK DITEMUKAN");
-        }
+                System.out.println("hasil = "+hasil);
+                break;
+            default:
+                System.out.println("operator"+operator+"tidak ditemukan");
 
+        }
     }
 }
