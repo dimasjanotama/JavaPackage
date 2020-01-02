6c38040c659f514a12f8f7265208c74350bffa90
dimasjanotama <dimasprasidha@gmail.com>
2020-01-02 19:36:00 +0700

    do while loop

--- src/com/loop/Main.java
+++ src/com/loop/Main.java
@@ -3,6 +3,7 @@ package com.loop;
 public class Main{
     public static void main(String[] args){
 
+        // WHILE LOOP
         int a = 0;
         boolean kondisi=true;
 
@@ -15,6 +16,21 @@ public class Main{
             }
         }
         System.out.println("------------AKHIR PROGRAM");
+
+        // DO WHILE LOOP
+        int b = 0;
+        boolean kondisi2 = true;
+
+        System.out.println("--------------INI AWAL PROGRAM");
+        do {
+            b++;
+            System.out.println(b);
+            if(b==10){
+                kondisi2 = false;
+            }
+        } while(kondisi2);
+
+        System.out.println("---------------INI AKHIR PROGRAM");
 
     }
 }
